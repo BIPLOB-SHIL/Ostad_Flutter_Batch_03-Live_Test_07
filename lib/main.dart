3438f01dfc6fe239ad14f9c24c303a0b2fdc302c
BIPLOB-SHIL <biplobshil.duet@gmail.com>
2023-06-19 23:47:39 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:livetest07/second_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CounterApp(),
+    );
+  }
+}
+
+class CounterApp extends StatefulWidget {
+  const CounterApp({super.key});
+
+  @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+
+  int counter=0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text("ProductList"),
+            centerTitle: true,
+            elevation: 5,
+            backgroundColor: Colors.amber,
+    ),
+        body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:  [
+                  Text("Cointer Value:"),
+                  Text("$counter"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Flexible(
+                        flex: 50,
+                          child: TextButton(
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                            ),
+                            onPressed: (){
+                              setState(() {
+                                counter = counter+1;
+                                if (counter >5) {
+                                  AlertDialog alert = AlertDialog(
+                                    title: const Text("Counter Alert!"),
+                                    content: Text("Counter value 5!"),
+                                  );
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return alert;
+                                    },
+                                  );
+                                }
+
+                                if (counter>=10){
+                                  Navigator.push(context,MaterialPageRoute(builder: (context) => SecondScreen(),));
+                                }
+
+
+                              });
+
+                            },
+                            child: Text("+",style: TextStyle(color: Colors.red),),
+                          )),
+                      Flexible(
+                          flex: 25,
+                          child: TextButton(
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                            ),
+                            onPressed: (){
+                              setState(() {
+                                counter = counter-1;
+                              });
+                            },
+                            child: Text("-",style: TextStyle(color: Colors.red),),
+                          ))
+                    ],
+                  )
+                ],
+
+              ),
+    ),
+
+    );
+  }
+}
+
+
+
+
+
